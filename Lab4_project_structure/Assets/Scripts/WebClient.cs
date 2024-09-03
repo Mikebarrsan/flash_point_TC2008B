091395b0281a100fcbb881433150f62784ba13d1
Johnccg <150750774+Johnccg@users.noreply.github.com>
2024-09-02 18:11:28 -0600

    Terminar generación de fuegos

--- Lab4_project_structure/Assets/Scripts/WebClient.cs
+++ Lab4_project_structure/Assets/Scripts/WebClient.cs
@@ -9,7 +9,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 using Newtonsoft.Json;
-using TMPro;
+using UnityEngine.SceneManagement;
 
 public class WebClient : MonoBehaviour
 {
@@ -106,14 +106,33 @@ public class WebClient : MonoBehaviour
             {
                 Debug.Log(www.downloadHandler.text);
                 Moves resp = JsonConvert.DeserializeObject<Moves>(www.downloadHandler.text.Replace('\'', '\"'));
+                bool delay = false;
                 for (int i = 0; i < resp.moves.Length; i++)
                 {
-                    for (int j = 0; j < resp.moves[i].Length; j++)
-                    {
-                        Debug.Log("[" + i + "] [" + j + "]" + resp.moves[i][j]);
-                        // if(resp.moves[i][j] == 2){
-                        //     Instantiate(FirePrefab, new Vector3(j * -1, 0, i), Quaternion.identity, FireHolder.transform);
-                        // }
+                    int x = int.Parse(resp.moves[i][2]) * -1;
+                    int z = int.Parse(resp.moves[i][1]);
+
+                    if (resp.moves[i][0] == "fire"){
+                        Collider[] intersecting = Physics.OverlapSphere(new Vector3(x, 0, z), 0.01f);
+                        if (intersecting.Length == 0) {
+                            Instantiate(FirePrefab, new Vector3(x, 0, z), Quaternion.Euler(-90,0,0), FireHolder.transform);
+                        }else{
+                            Destroy(intersecting[0].gameObject);
+                            Instantiate(FirePrefab, new Vector3(x, 0, z), Quaternion.Euler(-90,0,0), FireHolder.transform);
+                        }
+                    }else if (resp.moves[i][0] == "smoke"){
+                        Instantiate(SmokePrefab, new Vector3(x, 0, z), Quaternion.identity, FireHolder.transform);
+                    }else if (resp.moves[i][0] == "explosion"){
+                        Debug.Log("Explosión en " + resp.moves[i][1] + ", " + resp.moves[i][2]);
+                    }else if (resp.moves[i][0] == "flashover"){
+                        if (!delay){
+                            yield return new WaitForSeconds(2.0f);
+                            Debug.Log("Espero 2 segs");
+                            delay = true;
+                        }
+                        Collider[] intersecting = Physics.OverlapSphere(new Vector3(x, 0, z), 0.01f);
+                        Destroy(intersecting[0].gameObject);
+                        Instantiate(FirePrefab, new Vector3(x, 0, z), Quaternion.Euler(-90,0,0), FireHolder.transform);
                     }
                 }
             }
@@ -142,6 +161,8 @@ public class WebClient : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(getStep());
+        }else if (Input.GetKeyDown(KeyCode.Return)){
+            SceneManager.LoadScene("Default");
         }
     }
 }
